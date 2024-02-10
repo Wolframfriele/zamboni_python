19f5234bb479890811af5b2015fd1b848a8a4e64
Wolfram Friele <wolframfriele@gmail.com>
2024-02-10 22:56:39 +0100

    Allow deleting of words and of charecters.

--- test_environment.py
+++ test_environment.py
@@ -6,9 +6,21 @@ def ctrl(c: int) -> int:
     return (c) & 0x1F
 
 
+def buffer_to_string(buffer: list[list[int]], active: list[int]) -> str:
+    output: list[str] = []
+    for word in buffer:
+        for c in word:
+            output.append(chr(c))
+        output.append(" ")
+    for c in active:
+        output.append(chr(c))
+    return "".join(output)
+
+
 def main(window: curses.window) -> None:
     curses.raw()
-    buffer: list[int] = []
+    buffer: list[list[int]] = []
+    active: list[int] = []
     window.clear()
     # curses.init_pair(1, 243, curses.COLOR_BLACK)
     # stdscreen.addstr("predict the input", curses.color_pair(1))
@@ -16,16 +28,31 @@ def main(window: curses.window) -> None:
         c = window.getch()
         if c == ctrl(ord("q")):
             break
+        elif c == ord(" "):
+            buffer.append(active)
+            active = []
         elif c == curses.KEY_BACKSPACE:
             try:
-                buffer.pop()
+                active.pop()
             except IndexError:
-                pass
+                try:
+                    active = buffer.pop()
+                except IndexError:
+                    pass
+        elif c == 8:
+            if len(active) > 0:
+                active = []
+            else:
+                buffer.pop()
+        elif c == curses.KEY_LEFT:
+            if len(active) > 0:
+                active = []
+            else:
+                buffer.pop()
         else:
-            buffer.append(c)
-        write_line = "".join([chr(c) for c in buffer])
+            active.append(c)
         window.clear()
-        window.addstr(write_line)
+        window.addstr(buffer_to_string(buffer, active))
 
 
 curses.wrapper(main)
